959a938bfe62c1c0094f8a3fc0183aad6b05e3fb
flgina <71128309+flgina@users.noreply.github.com>
2022-10-22 05:50:51 -0400

    update

--- Tutorial 2/Assets/Scripts/PlayerScript.cs
+++ Tutorial 2/Assets/Scripts/PlayerScript.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class PlayerScript : MonoBehaviour
 {
@@ -9,15 +10,28 @@ public class PlayerScript : MonoBehaviour
 
     public float speed;
 
-    public Text score;
+    public int score;
+    public int lives;
 
-    private int scoreValue = 0;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI livesText;
+    public GameObject winTextObject;
+    public GameObject loseTextObject;
 
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
-        score.text = scoreValue.ToString();
+        score = 0;
+
+        rd2d = GetComponent<Rigidbody2D>();
+        lives = 3;
+
+        SetScoreText();
+        winTextObject.SetActive(false);
+
+        SetScoreText();
+        loseTextObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -32,9 +46,15 @@ public class PlayerScript : MonoBehaviour
     {
        if (collision.collider.tag == "Coin")
         {
-            scoreValue += 1;
-            score.text = scoreValue.ToString();
+            score += 1;
+            SetScoreText();
             Destroy(collision.collider.gameObject);
+        }
+
+        if (collision.collider.tag == "Enemy")
+        {
+            lives -= 1;
+            SetScoreText();
         }
 
     }
@@ -47,6 +67,21 @@ public class PlayerScript : MonoBehaviour
             {
                 rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse); //the 3 in this line of code is the player's "jumpforce," and you change that number to get different jump behaviors.  You can also create a public variable for it and then edit it in the inspector.
             }
+        }
+    }
+
+    private void SetScoreText()
+    {
+        scoreText.text = "Score: " + score.ToString();
+        if (score == 4)
+        {
+            winTextObject.SetActive(true);
+        }
+
+        livesText.text = "Lives: " + lives.ToString();
+        if (lives == 0)
+        {
+            loseTextObject.SetActive(true);
         }
     }
 }
